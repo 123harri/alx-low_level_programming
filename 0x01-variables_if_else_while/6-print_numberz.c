67c230df92c9f4e27ea9e15655975f471c652fa2
123harri <harrietwanjiku90@gmail.com>
2023-09-08 14:05:25 -0700

    6 print number

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: prints char below 10
+ * Return: Always 0 (success)
+ */
+int main(void)
+putchar('0');
+putchar('\n');
+return (0);
+}
