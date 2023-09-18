205caf321ac4da4ed5a92ae6779a866124526cda
123harri <harrietwanjiku90@gmail.com>
2023-09-18 13:50:28 -0700

    keygen

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,3 +1,4 @@
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
@@ -19,16 +20,16 @@ int main(void)
 
 	for (i = 0; i < 100; i++)
 	{
-	pass[i] = rand() % 78;
-	sum += (pass[i] + '0');
-	putchar(pass[i] + '0');
-	if ((2772 - sum) - '0' < 78)
-	{
-	n = 2772 - sum - '0';
-	sum += n;
-	putchar(n + '0');
-	break;
-	}
+		pass[i] = rand() % 78;
+		sum += (pass[i] + '0');
+		putchar(pass[i] + '0');
+		if ((2772 - sum) - '0' < 78)
+		{
+			n = 2772 - sum - '0';
+			sum += n;
+			putchar(n + '0');
+			break;
+		}
 	}
 
 	return (0);
