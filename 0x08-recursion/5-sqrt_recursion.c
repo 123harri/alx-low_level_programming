26134cc161f6de7ed63385271b9b200c35376acc
123harri <harrietwanjiku90@gmail.com>
2023-09-27 13:23:01 -0700

    sqrt recursion

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,6 +1,6 @@
 #include "main.h"
 
-int find_sqrt(int n, int start, int end);
+int actual_sqrt_recursion(int n, int i);
 
 /**
  * _sqrt_recursion - returns the natural square root of a number
@@ -10,44 +10,28 @@ int find_sqrt(int n, int start, int end);
 int _sqrt_recursion(int n)
 {
 if (n < 0)
+
+return (-1);
+return (actual_sqrt_recursion(n, 0));
+}
+/**
+ * actual_sqrt_recursion - a function that help to recursively
+ * find the square root in a range
+ * @n: number which the square root is calculated
+ * @i: iterator
+ * Return: natural square root of n, or -1 if no natural square root exist
+ */
+int actual_sqrt_recursion(int n, int i)
+{
+if (i * i > n)
 {
 return (-1);
 }
-else if (n == 0 || n == 1)
+
+if ((i * i) == n)
 {
-return (n);
+return (i);
 }
-else
-{
-return (find_sqrt(n, 1, n / 2));
-}
-}
-/**
- * find_sqrt - a function that help to recursively
- * find the square root in a range
- * @n: number which the square root is calculated
- * @start: starting point of the search range
- * @end: end point of the search range
- * Return: natural square root of n, or -1 if no natural square root exist
- */
-int find_sqrt(int n, int start, int end)
-{
-int mid;
-if (start > end)
-{
-return (end);
-}
-mid = (start + end) / 2;
-if ((mid *mid) == n)
-{
-return (mid);
-}
-else if ((mid *mid) < n)
-{
-return (find_sqrt(n, mid + 1, end));
-}
-else
-{
-return (find_sqrt(n, start, mid - 1));
-}
+
+return (actual_sqrt_recursion(n, i + 1));
 }
