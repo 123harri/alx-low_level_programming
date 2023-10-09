8da285ace08d63436bef6074cc797cb14845e27d
123harri <harrietwanjiku90@gmail.com>
2023-10-09 03:15:34 -0700

    File name

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - program that prints the name of the file
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	printf("The current source file is: %s\n", __FILE__);
+		return (0);
+}
