b31257456ab641a76a334c35ccd2e8d518ea6577
123harri <harrietwanjiku90@gmail.com>
2023-10-19 05:35:57 -0700

    advanced task

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -21,26 +21,26 @@ list_t *add_node_end(list_t **head, const char *str)
 		return (NULL);
 
 	new_node->str = strdup(str);
-	 if (new_node->str == NULL)
-	 {
-		 free(new_node);
-		 return (NULL);
-	 }
-	 new_node->len = strlen(str);
-	 new_node->next = NULL;
+	if (new_node->str == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+	new_node->len = strlen(str);
+	new_node->next = NULL;
 
-	 if (*head == NULL)
-	 {
-		 *head = new_node;
-	 }
-	 else
-	 {
-		 temp = *head;
-		 while (temp->next != NULL)
-		 {
-			 temp = temp->next;
-		 }
-		 temp->next = new_node;
-	 }
-	 return (new_node);
+	if (*head == NULL)
+	{
+		*head = new_node;
+	}
+	else
+	{
+		temp = *head;
+		while (temp->next != NULL)
+		{
+			temp = temp->next;
+		}
+		temp->next = new_node;
+	}
+	return (new_node);
 }
