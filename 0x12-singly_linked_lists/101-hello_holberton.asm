12c2d3c5a69abc9c5eff6a6a493ec85ab4b6f85c
123harri <harrietwanjiku90@gmail.com>
2023-10-19 05:48:39 -0700

    updated dvance task

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,16 +1,12 @@
+global main
+extern printf
+
 section .data
-	hello db "Hello, Holberton", 0xA
-	format db "%s", 0
-
+	format db 'Hello, Holberton\n', 0
 section .text
-	global main
-	extern printf
-
 main:
-	mov rdi, format
-	mov rsi, hello
-	call printf
-
-	mov rax, 60
-	xor rdi, rdi
-	syscall
+mov edi, format
+xor eax, eax
+call printf
+mov eax, 0
+ret
