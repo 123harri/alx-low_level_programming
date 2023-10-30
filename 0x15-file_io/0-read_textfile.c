04e71ce8cadbf6d7c883b09d8b3e68fd8cdaf28e
123harri <harrietwanjiku90@gmail.com>
2023-10-30 16:05:15 -0700

    second correction for task 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -24,15 +24,26 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (file_descriptor == -1)
 		return (0);
 
-	buffer = malloc(sizeof(char) * (letters));
+	buffer = malloc(sizeof(char) * (letters + 1));
 	if (!buffer)
 		return (0);
 
 	bytes_read = read(file_descriptor, buffer, letters);
+	if (bytes_read == -1)
+	{
+		free(buffer);
+		close(file_descriptor);
+		return (0);
+	}
+
 	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
-
+	if (bytes_written == -1)
+	{
+		free(buffer);
+		close(file_descriptor);
+		return (0);
+	}
 	close(file_descriptor);
-
 	free(buffer);
 
 	return (bytes_written);
