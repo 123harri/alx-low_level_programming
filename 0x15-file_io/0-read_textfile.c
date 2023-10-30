7f39099688b0905f783903a4b8868204561fb31d
123harri <harrietwanjiku90@gmail.com>
2023-10-30 15:42:28 -0700

    corrected task 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -16,36 +16,24 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	ssize_t bytes_read, bytes_written;
 	char *buffer;
 
-	if (filename == NULL)
-		return (0);
-
-	buffer = malloc(letters + 1);
-	if (buffer == NULL)
+	if (!filename)
 		return (0);
 
 	file_descriptor = open(filename, O_RDONLY);
-	if (file_descriptor == -1)
-	{
-		free(buffer);
-		return (0);
-	}
-	bytes_read = read(file_descriptor, buffer, letters);
-	if (bytes_read == -1)
-	{
-		free(buffer);
-		close(file_descriptor);
-		return (0);
-	}
-	buffer[bytes_read] = '\0';
 
-	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
-	if (bytes_written == -1 || bytes_written != bytes_read)
-	{
-		free(buffer);
-		close(file_descriptor);
+	if (file_descriptor == -1)
 		return (0);
-	}
-	free(buffer);
+
+	buffer = malloc(sizeof(char) * (letters));
+	if (!buffer)
+		return (0);
+
+	bytes_read = read(file_descriptor, buffer, letters);
+	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
+
 	close(file_descriptor);
+
+	free(buffer);
+
 	return (bytes_written);
 }
